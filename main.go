0b198d33e898e074f427c0bdfbd06c0aadac77d1
Isaque Veras <isaqueveras@alu.ufc.br>
2025-03-04 23:00:17 -0300

    feat: iniciando rota do backend

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/oklog/ulid/v2"
+)
 
 func main() {
-	fmt.Println("Olá mundo")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	r := gin.Default()
+
+	v1 := r.Group("v1")
+
+	tx := v1.Group("transaction")
+	tx.GET("", func(ctx *gin.Context) {
+		ctx.JSON(200, map[string]string{
+			"id":      ulid.Make().String(),
+			"product": "Arroz (1kg)",
+		})
+	})
+
+	r.Run()
 }
