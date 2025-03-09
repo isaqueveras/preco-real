4ffe3beb796fc2383acdc56ce8a04c23394a86bc
Isaque Veras <isaqueveras@alu.ufc.br>
2025-03-09 18:06:58 -0300

    feat: adiciona modal para filtrar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,50 @@
 import NextLink from "next/link"
-import { Card, Container, Link as ChakraLink, Table, Grid, GridItem } from "@chakra-ui/react"
-import { LuExternalLink } from "react-icons/lu";
+import { Card, Container, Link as ChakraLink, Table, Grid, GridItem, Tag, Box, Text } from "@chakra-ui/react"
+import { LuGlobe } from "react-icons/lu";
 import Menu from "@/components/menu";
 
 const items = [
-  { id: 1, name: "Arroz (5kg)", category: "Alimentos", variation: 0.2, price: 25, min: 20, max: 30 },
-  { id: 2, name: "Feijão (1kg)", category: "Alimentos", variation: 2, price: 8, min: 6, max: 12 },
+  { id: 1, name: "Arroz (5kg)", category: "Alimentos", variation: 0.2, price: "25,80", min: 20, max: 30 },
+  { id: 2, name: "Feijão (1kg)", category: "Alimentos", variation: -0.2, price: "8,00", min: 6, max: 12 },
   { id: 3, name: "Óleo de soja (900ml)", category: "Alimentos", variation: 1, price: 10, min: 8, max: 14 },
-  { id: 4, name: "Açúcar (5kg)", category: "Alimentos", variation: 0.4, price: 20, min: 18, max: 25 },
-  { id: 5, name: "Leite (1L)", category: "Alimentos", variation: 1.2, price: 6, min: 5, max: 8 },
+  { id: 4, name: "Açúcar (5kg)", category: "Alimentos", variation: -0.4, price: 20, min: 18, max: 25 },
+  { id: 5, name: "Leite (1L)", category: "Alimentos", variation: -1.2, price: 6, min: 5, max: 8 },
   { id: 6, name: "Café (500g)", category: "Alimentos", variation: 2.9, price: 15, min: 12, max: 18 },
-  { id: 7, name: "Pão francês (1kg)", category: "Alimentos", variation: 0.2, price: 15, min: 12, max: 18 },
+  { id: 7, name: "Pão francês (1kg)", category: "Alimentos", variation: -0.2, price: 15, min: 12, max: 18 },
   { id: 8, name: "Carne bovina (1kg)", category: "Carnes", variation: 0.2, price: 40, min: 35, max: 50 },
   { id: 9, name: "Frango (1kg)", category: "Carnes", variation: 0.2, price: 18, min: 15, max: 22 },
   { id: 10, name: "Ovos (dúzia)", category: "Alimentos", variation: 0.2, price: 12, min: 10, max: 15 },
-  { id: 11, name: "Batata (1kg)", category: "Hortifruti", variation: 0.2, price: 8, min: 6, max: 12 },
-  { id: 12, name: "Tomate (1kg)", category: "Hortifruti", variation: 0.2, price: 10, min: 8, max: 14 },
-  { id: 13, name: "Cebola (1kg)", category: "Hortifruti", variation: 0.2, price: 7, min: 5, max: 10 },
-  { id: 14, name: "Banana (1kg)", category: "Hortifruti", variation: 0.2, price: 8, min: 6, max: 12 },
-  { id: 15, name: "Sabão em pó (1kg)", category: "Limpeza", variation: 0.2, price: 18, min: 15, max: 22 },
-  { id: 16, name: "Detergente (500ml)", category: "Limpeza", variation: 0.2, price: 4, min: 3, max: 6 },
-  { id: 17, name: "Papel higiênico (4 rolos)", category: "Higiene", variation: 0.2, price: 10, min: 8, max: 14 },
-  { id: 18, name: "Creme dental (90g)", category: "Higiene", variation: 0.2, price: 6, min: 5, max: 9 },
-  { id: 19, name: "Sabonete (90g)", category: "Higiene", variation: 0.2, price: 3, min: 2, max: 5 },
-  { id: 20, name: "Shampoo (350ml)", category: "Higiene", variation: 0.2, price: 15, min: 12, max: 20 }
+  { id: 11, name: "Batata (1kg)", category: "Hortifruti", variation: -7.2, price: 8, min: 6, max: 12 },
+  { id: 12, name: "Tomate (1kg)", category: "Hortifruti", variation: -1.8, price: 10, min: 8, max: 14 },
+  { id: 13, name: "Cebola (1kg)", category: "Hortifruti", variation: 2.7, price: 7, min: 5, max: 10 },
+  { id: 14, name: "Banana (1kg)", category: "Hortifruti", variation: -3.5, price: 8, min: 6, max: 12 },
+  { id: 15, name: "Sabão em pó (1kg)", category: "Limpeza", variation: 3.4, price: 18, min: 15, max: 22 },
+  { id: 16, name: "Detergente (500ml)", category: "Limpeza", variation: 2.9, price: 4, min: 3, max: 6 },
+  { id: 17, name: "Papel higiênico (4 rolos)", category: "Higiene", variation: 7, price: 10, min: 8, max: 14 },
+  { id: 18, name: "Creme dental (90g)", category: "Higiene", variation: -9.2, price: 6, min: 5, max: 9 },
+  { id: 19, name: "Sabonete (90g)", category: "Higiene", variation: -2.2, price: 3, min: 2, max: 5 },
+  { id: 20, name: "Shampoo (350ml)", category: "Higiene", variation: 2.2, price: 15, min: 12, max: 20 }
 ];
 
 const news = [
-  { id: 1, title: "Tomate tem alta de 15% no mês de fevereiro", category: "Notícia", content: "O preço do tomate subiu devido à alta demanda e às condições climáticas desfavoráveis." },
-  { id: 2, title: "Leite fica mais barato com aumento da produção", category: "Notícia", content: "A produção de leite cresceu 8%, resultando em uma leve queda nos preços." },
-  { id: 3, title: "Feijão tem queda de preço após safra recorde", category: "Notícia", content: "A colheita deste ano superou expectativas, reduzindo os custos do feijão nos mercados." }
-  // { id: 4, title: "Consumo de café cresce 10% no último ano", category: "Notícia", content: "A colheita deste ano superou expectativas, reduzindo os custos do feijão nos mercados." }
+  {
+    id: 1,
+    title: "Tomate tem alta de 15% no mês de fevereiro",
+    content: "O preço do tomate subiu devido à alta demanda e às condições climáticas desfavoráveis.",
+    source: "g1.globo.com"
+  },
+  {
+    id: 2,
+    title: "Leite mais barato com aumento da produção",
+    content: "A produção de leite cresceu 8%, resultando em uma leve queda nos preços.",
+    source: "uol.com.br"
+  },
+  {
+    id: 3,
+    title: "Feijão tem queda de preço após safra recorde",
+    content: "A colheita deste ano superou expectativas, reduzindo os custos do feijão nos mercados.",
+    source: "cnnbrasil.com.br"
+  },
 ]
 
 export default function Home() {
@@ -54,20 +68,11 @@ export default function Home() {
                 <Card.Body gap="2">
                   <Card.Title fontSize={'md'}>
                     <ChakraLink asChild variant="underline">
-                      <NextLink href={`#noticia#${item.title}`}>
-                        {item.title}
-                      </NextLink>
-                    </ChakraLink>
+                      <NextLink href={`?${item.title}`}>{item.title}</NextLink></ChakraLink>
                   </Card.Title>
                   <Card.Description fontSize={'sm'}>{item.content}</Card.Description>
                   <Card.Footer p={0} fontSize={'sm'} color={'gray'}>
-                    <ChakraLink asChild variant="underline">
-                      <NextLink href={'#'}>
-                        <LuExternalLink />
-                        Fonte: g1.globo.com
-                      </NextLink>
-                    </ChakraLink>
-
+                    <LuGlobe />{item.source}
                   </Card.Footer>
                 </Card.Body>
               </Card.Root>
@@ -100,98 +105,27 @@ export default function Home() {
                         </ChakraLink>
                       </Table.Cell>
                       <Table.Cell>{item.category}</Table.Cell>
-                      <Table.Cell>R$ {item.price} (+{item.variation}%)</Table.Cell>
-                      <Table.Cell textAlign="end">R$ {item.price}</Table.Cell>
-                      <Table.Cell textAlign="left">R$ {item.price}</Table.Cell>
+                      <Table.Cell>
+                        R$ {item.price}
+                        <Tag.Root ml={1} size="sm" variant={'subtle'} colorPalette={item.variation >= 0 ? 'blue' : 'red'}>
+                          <Tag.Label>{item.variation}%</Tag.Label>
+                        </Tag.Root>
+                      </Table.Cell>
+                      <Table.Cell textAlign="end">R$ {item.min}</Table.Cell>
+                      <Table.Cell textAlign="left">R$ {item.max}</Table.Cell>
                     </Table.Row>
                   ))}
                 </Table.Body>
               </Table.Root>
             </Table.ScrollArea>
+
+            <Box display={'flex'} my={4} justifyContent={'space-between'}>
+              <Text fontSize={'sm'} color={'gray.500'}>Região: Mombaça no Ceará</Text>
+              <Text fontSize={'sm'} color={'gray.500'}>Dados do dia 03/03/25 à 05/03/25</Text>
+            </Box>
           </GridItem>
-
-          {/* <GridItem colSpan={1}>
-            <Heading pl={3} mb={2} color={'gray'} display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
-              <NextLink href="/tx">Transações</NextLink>
-              <NextLink href="#"><LuPackagePlus /></NextLink>
-            </Heading>
-            <TimelineRoot maxW="350px" pl={3}>
-              <TimelineItem >
-                <TimelineConnector mt={1}>
-                  <LuPackageX />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Transação Recusada</TimelineTitle>
-                  <TimelineDescription color={'gray'} fontSize={'sm'} pb={0}>Transação recusada por falta de comprovante.</TimelineDescription>
-                  <TimelineDescription>03 Mar 2025, 23:25</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-
-              <TimelineItem>
-                <TimelineConnector mt={1}>
-                  <LuPackageCheck />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Transação Aprovada</TimelineTitle>
-                  <TimelineDescription>03 Mar 2025, 23:23</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-
-              <TimelineItem >
-                <TimelineConnector mt={1}>
-                  <LuPackageX />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Transação Recusada</TimelineTitle>
-                  <TimelineDescription color={'gray'} fontSize={'sm'} pb={0}>Transação recusada por falta de comprovante.</TimelineDescription>
-                  <TimelineDescription>03 Mar 2025, 23:25</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-
-              <TimelineItem>
-                <TimelineConnector mt={1}>
-                  <LuPackageCheck />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Transação Aprovada</TimelineTitle>
-                  <TimelineDescription>03 Mar 2025, 23:23</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-
-              <TimelineItem >
-                <TimelineConnector mt={1}>
-                  <LuPackageX />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Transação Recusada</TimelineTitle>
-                  <TimelineDescription color={'gray'} fontSize={'sm'} pb={0}>Transação recusada por falta de comprovante.</TimelineDescription>
-                  <TimelineDescription>03 Mar 2025, 23:25</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-
-              <TimelineItem>
-                <TimelineConnector mt={1}>
-                  <LuPackageCheck />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Transação Aprovada</TimelineTitle>
-                  <TimelineDescription>03 Mar 2025, 23:23</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-
-              <TimelineItem>
-                <TimelineConnector mt={1}>
-                  <LuPackage />
-                </TimelineConnector>
-                <TimelineContent ml={-2}>
-                  <TimelineTitle textStyle="md">Nova Transação Recebida</TimelineTitle>
-                  <TimelineDescription>03 Mar 2025, 23:20</TimelineDescription>
-                </TimelineContent>
-              </TimelineItem>
-            </TimelineRoot>
-          </GridItem> */}
-        </Grid >
-      </Container >
+        </Grid>
+      </Container>
     </>
   )
 }
---
+++ frontend/src/components/filters.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText
+} from "@/components/ui/select"
+
+import { useRef, useState } from "react"
+import { Button, Dialog, Portal, createListCollection } from "@chakra-ui/react"
+
+export default function Filters() {
+  const contentRef = useRef<any>(null)
+
+  const [estado, setEstado] = useState<string[]>([])
+  const [cidade, setCidade] = useState<string[]>([])
+
+  return (
+    <Dialog.Root size={'xs'}>
+      <Dialog.Trigger asChild>
+        <Button variant="ghost" size={'md'}>Usar filtros</Button>
+      </Dialog.Trigger>
+
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner colorPalette={'pink'}>
+          <Dialog.Content ref={contentRef} rounded={1}>
+            <Dialog.Header>
+              <Dialog.Title>Filtrar Preços por Cidade e Estados</Dialog.Title>
+              <Dialog.Description mt={3}>Selecione um Estado e logo após selecione a cidade que deseja filtrar os preços.</Dialog.Description>
+            </Dialog.Header>
+            <Dialog.Body>
+
+              <SelectRoot mt={0} value={estado} collection={estados} colorPalette={'pink'} onValueChange={(e) => setEstado(e.value)}>
+                <SelectLabel>Selecione o Estado</SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText placeholder="Selecione o Estado" />
+                </SelectTrigger>
+                <SelectContent portalRef={contentRef}>
+                  {estados.items.map((item) => (<SelectItem item={item} key={item.label}>{item.label}</SelectItem>))}
+                </SelectContent>
+              </SelectRoot>
+
+              <SelectRoot mt={3} value={cidade} collection={cidades} colorPalette={'pink'} onValueChange={(e) => setCidade(e.value)}>
+                <SelectLabel>Selecione a Cidade</SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText placeholder="Selecione o Estado" />
+                </SelectTrigger>
+                <SelectContent portalRef={contentRef}>
+                  {cidades.items.map((item) => (<SelectItem item={item} key={item.label}>{item.label}</SelectItem>))}
+                </SelectContent>
+              </SelectRoot>
+
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button size={'sm'} variant="outline" rounded={1}>Fechar</Button>
+              </Dialog.ActionTrigger>
+              <Button size={'sm'} colorPalette={'pink'} rounded={1}>Aplicar filtros</Button>
+            </Dialog.Footer>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root >
+  )
+}
+
+const estados = createListCollection({
+  items: [
+    { value: "AC", label: "Acre" },
+    { value: "AL", label: "Alagoas" },
+    { value: "AP", label: "Amapá" },
+    { value: "AM", label: "Amazonas" },
+    { value: "BA", label: "Bahia" },
+    { value: "CE", label: "Ceará" },
+    { value: "DF", label: "Distrito Federal" },
+    { value: "ES", label: "Espírito Santo" },
+    { value: "GO", label: "Goiás" },
+    { value: "MA", label: "Maranhão" },
+    { value: "MT", label: "Mato Grosso" },
+    { value: "MS", label: "Mato Grosso do Sul" },
+    { value: "MG", label: "Minas Gerais" },
+    { value: "PA", label: "Pará" },
+    { value: "PB", label: "Paraíba" },
+    { value: "PR", label: "Paraná" },
+    { value: "PE", label: "Pernambuco" },
+    { value: "PI", label: "Piauí" },
+    { value: "RJ", label: "Rio de Janeiro" },
+    { value: "RN", label: "Rio Grande do Norte" },
+    { value: "RS", label: "Rio Grande do Sul" },
+    { value: "RO", label: "Rondônia" },
+    { value: "RR", label: "Roraima" },
+    { value: "SC", label: "Santa Catarina" },
+    { value: "SP", label: "São Paulo" },
+    { value: "SE", label: "Sergipe" },
+    { value: "TO", label: "Tocantins" }
+  ]
+})
+
+const cidades = createListCollection({
+  items: [
+    { value: "1", label: "Fortaleza" },
+    { value: "2", label: "Juazeiro do Norte" },
+    { value: "3", label: "Sobral" },
+    { value: "4", label: "Caucaia" },
+    { value: "5", label: "Crato" },
+    { value: "6", label: "Maracanaú" },
+    { value: "7", label: "Quixadá" },
+    { value: "8", label: "Itapipoca" },
+    { value: "9", label: "Iguatu" },
+    { value: "10", label: "Mombaça" }
+  ]
+})
