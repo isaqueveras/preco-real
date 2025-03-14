3dc5bc20a68b0bac66097d81fa2bad038c8b8605
Isaque Veras <isaqueveras@alu.ufc.br>
2025-03-14 20:53:31 -0300

    feat: fazendo requisição para o backend para listar os principais produtos

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,51 +3,26 @@ import { Card, Container, Link as ChakraLink, Table, Grid, GridItem, Tag, Box, T
 import { LuGlobe } from "react-icons/lu";
 import Menu from "@/components/menu";
 
-const items = [
-  { id: 1, name: "Arroz (5kg)", category: "Alimentos", variation: 0.2, price: "25,80", min: 20, max: 30 },
-  { id: 2, name: "Feijão (1kg)", category: "Alimentos", variation: -0.2, price: "8,00", min: 6, max: 12 },
-  { id: 3, name: "Óleo de soja (900ml)", category: "Alimentos", variation: 1, price: 10, min: 8, max: 14 },
-  { id: 4, name: "Açúcar (5kg)", category: "Alimentos", variation: -0.4, price: 20, min: 18, max: 25 },
-  { id: 5, name: "Leite (1L)", category: "Alimentos", variation: -1.2, price: 6, min: 5, max: 8 },
-  { id: 6, name: "Café (500g)", category: "Alimentos", variation: 2.9, price: 15, min: 12, max: 18 },
-  { id: 7, name: "Pão francês (1kg)", category: "Alimentos", variation: -0.2, price: 15, min: 12, max: 18 },
-  { id: 8, name: "Carne bovina (1kg)", category: "Carnes", variation: 0.2, price: 40, min: 35, max: 50 },
-  { id: 9, name: "Frango (1kg)", category: "Carnes", variation: 0.2, price: 18, min: 15, max: 22 },
-  { id: 10, name: "Ovos (dúzia)", category: "Alimentos", variation: 0.2, price: 12, min: 10, max: 15 },
-  { id: 11, name: "Batata (1kg)", category: "Hortifruti", variation: -7.2, price: 8, min: 6, max: 12 },
-  { id: 12, name: "Tomate (1kg)", category: "Hortifruti", variation: -1.8, price: 10, min: 8, max: 14 },
-  { id: 13, name: "Cebola (1kg)", category: "Hortifruti", variation: 2.7, price: 7, min: 5, max: 10 },
-  { id: 14, name: "Banana (1kg)", category: "Hortifruti", variation: -3.5, price: 8, min: 6, max: 12 },
-  { id: 15, name: "Sabão em pó (1kg)", category: "Limpeza", variation: 3.4, price: 18, min: 15, max: 22 },
-  { id: 16, name: "Detergente (500ml)", category: "Limpeza", variation: 2.9, price: 4, min: 3, max: 6 },
-  { id: 17, name: "Papel higiênico (4 rolos)", category: "Higiene", variation: 7, price: 10, min: 8, max: 14 },
-  { id: 18, name: "Creme dental (90g)", category: "Higiene", variation: -9.2, price: 6, min: 5, max: 9 },
-  { id: 19, name: "Sabonete (90g)", category: "Higiene", variation: -2.2, price: 3, min: 2, max: 5 },
-  { id: 20, name: "Shampoo (350ml)", category: "Higiene", variation: 2.2, price: 15, min: 12, max: 20 }
-];
-
 const news = [
-  {
-    id: 1,
-    title: "Tomate tem alta de 15% no mês de fevereiro",
-    content: "O preço do tomate subiu devido à alta demanda e às condições climáticas desfavoráveis.",
-    source: "g1.globo.com"
-  },
-  {
-    id: 2,
-    title: "Leite mais barato com aumento da produção",
-    content: "A produção de leite cresceu 8%, resultando em uma leve queda nos preços.",
-    source: "uol.com.br"
-  },
-  {
-    id: 3,
-    title: "Feijão tem queda de preço após safra recorde",
-    content: "A colheita deste ano superou expectativas, reduzindo os custos do feijão nos mercados.",
-    source: "cnnbrasil.com.br"
-  },
+  { id: 1, title: "Tomate tem alta de 15% no mês de fevereiro", content: "O preço do tomate subiu devido à alta demanda e às condições climáticas desfavoráveis.", source: "g1.globo.com" },
+  { id: 2, title: "Leite mais barato com aumento da produção", content: "A produção de leite cresceu 8%, resultando em uma leve queda nos preços.", source: "uol.com.br" },
+  { id: 3, title: "Feijão tem queda de preço após safra recorde", content: "A colheita deste ano superou expectativas, reduzindo os custos do feijão nos mercados.", source: "cnnbrasil.com.br" },
 ]
 
-export default function Home() {
+export const revalidate = 60
+
+interface Produto {
+  produto: string
+  categoria: string
+  preco: number
+  variacao: number
+  min: number
+  max: number
+}
+
+export default async function Page() {
+  const data = await fetch('http://localhost:8080/v1/consulta/obter_principais_produtos')
+  const produtos: Produto[] = await data.json()
   return (
     <>
       <Menu />
@@ -97,18 +72,18 @@ export default function Home() {
                 </Table.Header>
 
                 <Table.Body padding={1} >
-                  {items.map((item) => (
+                  {produtos.map((item: any) => (
                     <Table.Row key={item.id}>
                       <Table.Cell fontWeight={'normal'}>
                         <ChakraLink asChild variant="plain">
-                          <NextLink href={`#produto#${item.name}`}>{item.name}</NextLink>
+                          <NextLink href={`#produto#${item.produto}`}>{item.produto}</NextLink>
                         </ChakraLink>
                       </Table.Cell>
-                      <Table.Cell>{item.category}</Table.Cell>
+                      <Table.Cell>{item.categoria}</Table.Cell>
                       <Table.Cell>
-                        R$ {item.price}
-                        <Tag.Root ml={1} size="sm" variant={'subtle'} colorPalette={item.variation >= 0 ? 'blue' : 'red'}>
-                          <Tag.Label>{item.variation}%</Tag.Label>
+                        R$ {item.preco}
+                        <Tag.Root ml={1} size="sm" variant={'subtle'} colorPalette={item.variacao >= 0 ? 'blue' : 'red'}>
+                          <Tag.Label>{item.variacao}%</Tag.Label>
                         </Tag.Root>
                       </Table.Cell>
                       <Table.Cell textAlign="end">R$ {item.min}</Table.Cell>
